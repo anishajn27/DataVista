f355928cd759586a5cdfebbac30972b4c4ea59f5
Anisha Jain <anishajain2703@gmail.com>
2024-11-13 10:03:58 +0530

    Fixed Hamburger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,6 +18,9 @@ export default function Navbar() {
       </button>
       <ul className={`menu ${isMenuOpen ? "open" : ""}`}>
         <li>
+          <a href="#">     </a>
+        </li>
+        <li>
           <a href="#home">Home</a>
         </li>
         <li className="submenu-container">
